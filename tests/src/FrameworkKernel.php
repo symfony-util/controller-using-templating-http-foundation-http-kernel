208f4d9b4024c3fd1b869db945dec1481269f7d5
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-22 10:51:27 -0400

    Update FrameworkKernel.php

--- tests/src/FrameworkKernel.php
+++ tests/src/FrameworkKernel.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
