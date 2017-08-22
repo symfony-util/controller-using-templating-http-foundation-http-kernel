170d59ac411864ba4c36b5b83487de63d21fdb45
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-22 13:17:03 -0400

    Update FrameworkKernel.php

--- tests/src/FrameworkKernel.php
+++ tests/src/FrameworkKernel.php
@@ -56,7 +56,7 @@ class FrameworkKernel extends Kernel
 
         // Extensions
         $c->loadFromExtension('framework', [
-            // 'secret' => 'NotSecret', // What about use $ uuid -v4  or $ uuidgen
+            'secret' => 'NotSecret', // What about use $ uuid -v4  or $ uuidgen
             'test' => in_array($this->getEnvironment(), ['test'], true), // test.client service for eg. PHPUnit
             'profiler' => ['enabled' => in_array($this->getEnvironment(), ['dev', 'test'], true)],
             'templating' => [
