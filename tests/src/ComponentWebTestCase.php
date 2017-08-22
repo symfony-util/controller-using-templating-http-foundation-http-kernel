f5ba5ce1db7127d6c09adbda18aee52f14d15d3f
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-22 16:13:03 -0400

    Create ComponentWebTestCase.php

---
+++ tests/src/ComponentWebTestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ComponentWebTestCase extends WebTestCase
+{
+    protected static function getKernelClass()
+    {
+        return ComponentKernel::class;
+    }
+}
