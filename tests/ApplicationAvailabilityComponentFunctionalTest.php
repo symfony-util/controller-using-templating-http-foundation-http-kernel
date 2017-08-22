32891f51c15537175689178a70f55a890abc7b06
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-22 16:15:34 -0400

    Create ApplicationAvailabilityComponentFunctionalTest.php

---
+++ tests/ApplicationAvailabilityComponentFunctionalTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests;
+
+class ApplicationAvailabilityComponentFunctionalTest extends \ComponentWebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function urlProvider()
+    {
+        return [
+            ['/'],
+        ];
+    }
+}
