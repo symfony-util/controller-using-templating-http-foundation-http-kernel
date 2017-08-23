6968d68495f767f932ed1f6c2b42667d007ad4ba
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-23 15:13:50 -0400

    Update EngineAsArgumentInKernelControllerTest.php

--- tests/Component/EngineAsArgumentInKernelControllerTest.php
+++ tests/Component/EngineAsArgumentInKernelControllerTest.php
@@ -11,8 +11,19 @@
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Bridge\Twig\TwigEngine;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\ResponseListener;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+use SymfonyUtil\Controller\EngineAsArgumentController;
 use Tests\Component\AppKernel;
 
 /**
@@ -38,12 +49,38 @@ final class EngineAsArgumentInKernelControllerTest extends TestCase
         );
     }
 
-    public function testReturnsResponse()
+    public function testFrameworkReturnsResponse()
     {
         $this->assertInstanceOf(
             // Response::class, // 5.4 < php
             'Symfony\Component\HttpFoundation\Response',
             (new AppKernel('dev', true))->handle(Request::create('/', 'GET'))
+        );
+    }
+
+    public function testComponentReturnsResponse()
+    {
+        $requestStack = new RequestStack();
+        $routes = new RouteCollectionBuilder(); // Because I know how to use it.
+        $routes->add('/', EngineAsArgumentController::class, 'index'); // returns Symfony/Component/Routing/Route
+        $dispatcher = new EventDispatcher();
+        $dispatcher->addSubscriber(new RouterListener(
+            new UrlMatcher(
+                $routes-> build(), 
+                new RequestContext()
+            ),
+            $requestStack
+        ));
+        $dispatcher->addSubscriber(new ResponseListener('UTF-8'));
+        $this->assertInstanceOf(
+            // Response::class, // 5.4 < php
+            'Symfony\Component\HttpFoundation\Response',
+            (new HttpKernel(
+                $dispatcher,
+                new ControllerResolver(),
+                $requestStack,
+                new ArgumentResolver();
+            ))->handle(Request::create('/', 'GET'))
         );
     }
 }
