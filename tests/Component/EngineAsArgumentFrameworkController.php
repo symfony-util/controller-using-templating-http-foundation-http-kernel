2b808cc6c191b342b7207dc1035cc3678ba64e31
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-24 16:35:20 -0400

    Create EngineAsArgumentFrameworkController.php

---
+++ tests/Component/EngineAsArgumentFrameworkController.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymfonyUtil\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\EngineInterface;
+
+class EngineAsArgumentFrameworkController extends Controller
+{
+    public function __construct($template = 'index.html.twig')
+    {
+        $this->template = $template;
+    }
+
+    public function __invoke(EngineInterface $templating) // symfony >= 3.3
+    {
+        return new Response($templating->render($this->template));
+    }
+}
