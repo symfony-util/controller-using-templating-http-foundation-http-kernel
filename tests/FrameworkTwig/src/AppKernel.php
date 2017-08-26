b25830ce0da9b39ecd9c7bf8c7fcd6ded7fde414
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-26 11:17:43 -0400

    Update AppKernel.php

--- tests/FrameworkTwig/src/AppKernel.php
+++ tests/FrameworkTwig/src/AppKernel.php
@@ -48,7 +48,7 @@ class AppKernel extends Kernel
 
         $c->autowire(TemplatingController::class)
             ->setAutoconfigured(true)
-            ->addTag('controller.service_arguments')
+            // ->addTag('controller.service_arguments')
             ->setPublic(true);
 
         // Extensions
