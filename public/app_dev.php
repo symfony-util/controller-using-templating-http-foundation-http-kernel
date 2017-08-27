cc7455c99172bccbe9c9fe90cfa3ca1299cfa546
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-26 21:36:41 -0400

    Update app_dev.php

--- public/app_dev.php
+++ public/app_dev.php
@@ -9,8 +9,8 @@
  * file that was distributed with this source code.
  */
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
@@ -21,13 +21,13 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || PHP_SAPI === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-chdir('..') ; // TODO: is this needed and why? -- to find autoload, kernel.root_dir and config file
+chdir('..'); // TODO: is this needed and why? -- to find autoload, kernel.root_dir and config file
 
 $loader = require 'vendor/autoload.php';
 // $loader = require __DIR__.'/../vendor/autoload.php';
