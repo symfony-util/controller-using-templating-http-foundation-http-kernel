f502207bfa7470e713a6c6ee75398f2d0c30e47e
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-26 21:07:21 -0400

    Update app_dev.php

--- public/app_dev.php
+++ public/app_dev.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
